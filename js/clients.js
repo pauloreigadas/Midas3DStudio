76cdc7931e71e2c651e2de678beeefa2da4ff1d5
Paulo Reigadas <paulo.reigadas@gmail.com>
2019-04-10 14:52:00 -0700

    including pictures

---
+++ js/clients.js
@@ -0,0 +1,16 @@
+$(function(){
+	$(".carousel-clients").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true
+
+
+
+
+	});
+  $(".slick-slide").css("display", "flex");
+});
