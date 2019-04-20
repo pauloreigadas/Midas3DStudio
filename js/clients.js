e20dd6f8f3ba69eea7c64207c799501112fad4a7
Paulo Reigadas <paulo.reigadas@gmail.com>
2019-04-20 10:18:08 -0700

    add pictures

--- js/clients.js
+++ js/clients.js
@@ -7,30 +7,39 @@ $(function(){
     slidesToScroll:1,
     autoplay: true,
     autoplaySpeed: 2000,
-    dots: true
+    dots: true,
 
-	  // responsive: [
-	  //   {
-	  //     breakpoint: 768,
-	  //     settings: {
-	  //       arrows: false,
-	  //       centerMode: true,
-	  //       centerPadding: '40px',
-	  //       slidesToShow: 3
-	  //     }
-	  //   },
-	  //   {
-	  //     breakpoint: 480,
-	  //     settings: {
-	  //       arrows: false,
-	  //       centerMode: true,
-	  //       centerPadding: '40px',
-	  //       slidesToShow: 1
-	  //     }
-	  //   }
-	  // ]
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        // arrows: false,
+	        centerMode: true,
+	        centerPadding: '00px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        // arrows: false,
+	        centerMode: true,
+	        centerPadding: '00px',
+	        slidesToShow: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        // arrows: false,
+	        centerMode: true,
+	        centerPadding: '0px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
 
 
 	});
-  // $(".slick-slide").css("display", "flex");
+  $(".slick-slide").css("display", "flex");
 });
